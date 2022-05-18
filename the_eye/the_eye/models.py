224131aac2a10395420c7348419adecf144e2446
Arnold Lestin <amlestin@gmail.com>
2022-05-17 20:24:02 -0500

    Updated timestamp field

--- the_eye/the_eye/models.py
+++ the_eye/the_eye/models.py
@@ -5,7 +5,7 @@ class Event(models.Model):
     session_id = models.CharField(max_length=150)
     category = models.CharField(max_length=150)
     data = models.JSONField()
-    timestamp = models.TimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['timestamp']
