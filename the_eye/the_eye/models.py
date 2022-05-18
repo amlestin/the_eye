990fe755f4a7518c45aaa112136063db4b74332b
Arnold Lestin <amlestin@gmail.com>
2022-05-17 20:10:08 -0500

    event model

---
+++ the_eye/the_eye/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create event model
+class Event(models.Model):
+    session_id = models.CharField(max_length=150)
+    category = models.CharField(max_length=150)
+    data = models.JSONField
+    timestamp = models.TimeField
